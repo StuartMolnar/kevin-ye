a9aaeab51292b60b1e35a4d8b2fbeaade95f0570
StuartMolnar <stuartmolnar@hotmail.com>
2023-06-15 20:54:40 -0700

    strength training desktop

---
+++ components/Copyright.tsx
@@ -0,0 +1,10 @@
+function Copyright() {
+    const currentYear = new Date().getFullYear();
+  
+    return (
+      <p>© Copyright {currentYear} Kevin Ye.</p>
+    );
+  }
+  
+  export default Copyright;
+  
