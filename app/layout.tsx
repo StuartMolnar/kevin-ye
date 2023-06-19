41eabfc3f86902812c990a86a2a55d5949bfc226
StuartMolnar <stuartmolnar@hotmail.com>
2023-06-18 23:04:30 -0700

    remove metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Kevin Ye',
-  description: 'placeholder description',
-}
-
 export default function RootLayout({
   children,
 }: {
