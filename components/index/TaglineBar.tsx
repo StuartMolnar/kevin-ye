01bd3f8844e8465e1830c2352c3faa8073d81f41
StuartMolnar <stuartmolnar@hotmail.com>
2023-07-29 01:30:21 -0700

    Update TaglineBar.tsx

--- components/index/TaglineBar.tsx
+++ components/index/TaglineBar.tsx
@@ -3,7 +3,7 @@ function TaglineBar() {
     return (
         <section className="bg-gold w-full py-[40px] md:py-[70px] px-[20px] md:px-10 flex flex-col justify-center items-center transition-all duration-200 ease-in-out">
         <h3 className="font-medium text-center text-h3 md:text-h3-dsk max-w-[900px]">
-          "I specialize in helping people discover their passion for fitness and help develop the right program catered just for you."
+          &quot;I specialize in helping people discover their passion for fitness and help develop the right program catered just for you.&quot;
         </h3>
       </section>
     );
