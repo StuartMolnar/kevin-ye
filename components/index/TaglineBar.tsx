50f52bd123557fb22a4f7dd07d1b62321cb77a24
StuartMolnar <stuartmolnar@hotmail.com>
2023-07-29 01:11:47 -0700

    add tagline content

--- components/index/TaglineBar.tsx
+++ components/index/TaglineBar.tsx
@@ -3,7 +3,7 @@ function TaglineBar() {
     return (
         <section className="bg-gold w-full py-[40px] md:py-[70px] px-[20px] md:px-10 flex flex-col justify-center items-center transition-all duration-200 ease-in-out">
         <h3 className="font-medium text-center text-h3 md:text-h3-dsk max-w-[900px]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non tellus ut neque aliquet vehicula quis vitae orci.
+          "I specialize in helping people discover their passion for fitness and help develop the right program catered just for you."
         </h3>
       </section>
     );
